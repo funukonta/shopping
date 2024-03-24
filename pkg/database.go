cf225ff613b2c28b225f7113fbab31527ef75315
funukonta <evanroy36@gmail.com>
2024-03-24 19:44:35 +0700

    feat : add db, add env

---
+++ pkg/database.go
@@ -0,0 +1,34 @@
+package pkg
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func ConnectPostgre() (*sqlx.DB, error) {
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	dbname := os.Getenv("DB_NAME")
+	ssl := os.Getenv("DB_SSL")
+
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", user, password, host, dbname, ssl)
+
+	db, err := sqlx.Open("postgres", connStr)
+	if err != nil {
+		log.Panicln("error connStr", err.Error())
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Panicln("error ping", err.Error())
+	}
+
+	return db, nil
+}
