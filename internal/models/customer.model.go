7b0308a1dda721cfa146271310baf2a6dc5e5e51
funukonta <evanroy36@gmail.com>
2024-03-25 02:43:01 +0700

    fix : change time.Time to pointer and add omitempty to id and times

--- internal/models/customer.model.go
+++ internal/models/customer.model.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type CustomerModel struct {
-	ID        int       `json:"id" db:"id"`
-	Name      string    `json:"name" db:"name"`
-	Email     string    `json:"email" db:"email"`
-	Password  string    `json:"password" db:"password"`
-	Address   string    `json:"address" db:"address"`
-	Phone     string    `json:"phone" db:"phone"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	ID        int        `json:"id,omitempty" db:"id"`
+	Name      string     `json:"name" db:"name"`
+	Email     string     `json:"email" db:"email"`
+	Password  string     `json:"password" db:"password"`
+	Address   string     `json:"address" db:"address"`
+	Phone     string     `json:"phone" db:"phone"`
+	CreatedAt *time.Time `json:"created_at,omitempty" db:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty" db:"updated_at"`
 }
 
 /*
