48198345390aa851db7c54f8e2030e366d7299f8
funukonta <evanroy36@gmail.com>
2024-03-25 02:27:05 +0700

    feat : add routes

---
+++ internal/routes/customer.route.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/funukonta/shopping/internal/handlers"
+	"github.com/funukonta/shopping/internal/repositories"
+	"github.com/funukonta/shopping/internal/services"
+	"github.com/jmoiron/sqlx"
+)
+
+func CustomerRoutes(mux *http.ServeMux, db *sqlx.DB) {
+	repo := repositories.NewCustomerRepo(db)
+	service := services.NewCustomerService(repo)
+	customerHandler := handlers.NewCustomerHandler(service)
+
+	mux.Handle("POST /customers", Handler(customerHandler.Create))
+}
---
+++ internal/routes/routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/funukonta/shopping/pkg"
+	"github.com/jmoiron/sqlx"
+)
+
+func Routes(mux *http.ServeMux, db *sqlx.DB) {
+	CustomerRoutes(mux, db)
+}
+
+type Handler func(w http.ResponseWriter, r *http.Request) error
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := h(w, r)
+	if err != nil {
+		pkg.Response(400, &pkg.JsonBod{Message: err.Error()}).Send(w)
+	}
+}
